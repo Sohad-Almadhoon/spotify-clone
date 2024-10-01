ae57cde2434e7ece4cbd88ae4298068756c9f934
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-10-01 17:50:24 +0300

    final-check

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,10 +52,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             justify-between
         ">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
             <RxCaretLeft className="text-white" size={35} />
           </button>
-          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
             <RxCaretRight className="text-white" size={35} />
           </button>
         </div>
